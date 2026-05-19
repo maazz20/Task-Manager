d446a0bf11c53d1f568dca4ce752018db344f506
maazz20 <maazjaved1409@gmail.com>
2026-05-19 08:49:22 +0530

    Disabled datasource and Hibernate

--- src/main/java/project/taskManager/TaskManagerApplication.java
+++ src/main/java/project/taskManager/TaskManagerApplication.java
@@ -2,8 +2,15 @@ package project.taskManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+    }
+)
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
